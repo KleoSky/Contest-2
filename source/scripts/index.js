d672738299201106e21a17430fbe0939d3b34b35
KleoSky <V-Kleo@yandex.ru>
2025-12-01 12:35:00 +0700

    Начинает блок Portfolio

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,7 @@
 import { initMenu } from './modules/burger-menu';
+import { renderProjects } from './modules/projects-render';
 
 window.addEventListener('DOMContentLoaded', () => {
   initMenu();
+  renderProjects();
 });
