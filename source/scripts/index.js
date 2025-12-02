9f396bce927264b14e5f9a29f94d0d15f965ef18
KleoSky <161670250+KleoSky@users.noreply.github.com>
2025-12-02 21:57:38 +0700

    Добавляет блок Portfolio (#2)
    * Начинает блок Portfolio
    
    * Полностью добавляет блок Portfolio

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,7 @@
 import { initMenu } from './modules/burger-menu';
+import { initProjectsReorganizer } from './modules/projects-reorganizer';
 
 window.addEventListener('DOMContentLoaded', () => {
   initMenu();
+  initProjectsReorganizer();
 });
